bd8e35fcef65286d242525c1901e033ae64c1249
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2022-01-03 19:18:04 +0900

    ava testing

---
+++ tests/pathQL.test.ts
@@ -0,0 +1,134 @@
+import test from "ava";
+
+import {
+  regularExpression,
+  seq,
+  or,
+  kleeneClojure,
+  empty,
+  lazy,
+  Expression,
+} from "../lib/main";
+
+const identifier = () => regularExpression(/\w*/);
+
+const attributeName = identifier();
+const numberAttributeValue = regularExpression(/[\d]+/);
+const stringAttributeValue = regularExpression(/[^']+/);
+const falseAttributeValue = seq`false`;
+const trueAttributeValue = seq`true`;
+const booleanAttributeValue = or(trueAttributeValue, falseAttributeValue);
+const attributeValue = or(
+  seq`'${stringAttributeValue}'`,
+  booleanAttributeValue,
+  numberAttributeValue
+);
+const predicate = seq`${attributeName}${or(seq`(${attributeValue})`, empty)}`;
+const predicatesUnion = seq`${predicate}${kleeneClojure(seq`+${predicate}`)}`;
+const predicatesIntersection: Expression = seq`:${predicatesUnion}${or(
+  lazy(() => predicatesIntersection),
+  empty
+)}`;
+const entityName = identifier();
+const pathQL: Expression = seq`/${entityName}${or(
+  predicatesIntersection,
+  empty
+)}${or(
+  lazy(() => pathQL),
+  empty
+)}`;
+
+const parseResult = pathQL.parse(
+  "/todos:completed+inProgress:id('12345')/piyo"
+);
+
+test("Correctly parse the pathQL string.", (t) => {
+  t.deepEqual(parseResult, {
+    type: "success",
+    node: {
+      expressionType: pathQL.type,
+      type: "internal",
+      children: [
+        {
+          expressionType: entityName.type,
+          type: "leaf",
+          value: "todos",
+        },
+        {
+          expressionType: predicatesIntersection.type,
+          type: "internal",
+          children: [
+            {
+              expressionType: predicatesUnion.type,
+              type: "internal",
+              children: [
+                {
+                  expressionType: predicate.type,
+                  type: "internal",
+                  children: [
+                    {
+                      expressionType: attributeName.type,
+                      type: "leaf",
+                      value: "completed",
+                    },
+                  ],
+                },
+                {
+                  expressionType: predicate.type,
+                  type: "internal",
+                  children: [
+                    {
+                      expressionType: attributeName.type,
+                      type: "leaf",
+                      value: "inProgress",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              expressionType: predicatesIntersection.type,
+              type: "internal",
+              children: [
+                {
+                  expressionType: predicatesUnion.type,
+                  type: "internal",
+                  children: [
+                    {
+                      expressionType: predicate.type,
+                      type: "internal",
+                      children: [
+                        {
+                          expressionType: attributeName.type,
+                          type: "leaf",
+                          value: "id",
+                        },
+                        {
+                          expressionType: stringAttributeValue.type,
+                          type: "leaf",
+                          value: "12345",
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          expressionType: pathQL.type,
+          type: "internal",
+          children: [
+            {
+              expressionType: entityName.type,
+              type: "leaf",
+              value: "piyo",
+            },
+          ],
+        },
+      ],
+    },
+    state: { index: 44 },
+  });
+});
